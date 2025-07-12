3d6c243a430a9e9a5e5b160212cb934fa279e3a9
PrinceOnfire890 <princehere2010@gmail.com>
2025-07-12 12:14:56 +0530

    Update joinNoti.js

--- Rudra/events/joinNoti.js
+++ Rudra/events/joinNoti.js
@@ -15,10 +15,10 @@ module.exports.onLoad = function () {
   const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
   const { join } = global.nodemodule["path"];
 
-  const path = join(__dirname, "cache", "joinvideo");
+  const path = join(__dirname, "cache", "joinvieo");
   if (existsSync(path)) mkdirSync(path, { recursive: true }); 
 
-  const path2 = join(__dirname, "cache", "joinvideo", "randomgif");
+  const path2 = join(__dirname, "cache", "joinvieo", "randomgif");
   if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
 
   return;
@@ -32,7 +32,7 @@ module.exports.run = async function({ api, event }) {
       api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? " " : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
       const fs = require("fs");
       return api.sendMessage("", event.threadID, () => api.sendMessage({body: `(𝐋𝐎 𝐌𝐀𝐈 𝐀𝐀 𝐆𝐘𝐘𝐀 𝐀𝐀𝐏𝐊𝐈 ✦𝐇𝐀𝐍𝐙𝐀𝐋𝐀✦ 𝐉𝐀𝐋𝐃𝐈 𝐒𝐄 𝐒𝐖𝐀𝐆𝐀𝐓 𝐊𝐀𝐑𝐎 𝐇𝐌𝐀𝐑𝐀 😀👈\n●============================================●\n\n𝐎𝐑 𝐌𝐄𝐑𝐄 𝐏𝐘𝐀𝐑𝐄 𝐃𝐎𝐒𝐓𝐎 𝐉𝐀𝐋𝐃𝐈 𝐒𝐄 𝐌𝐄𝐑𝐈 𝐁𝐎𝐒𝐒 𝐇𝐀𝐍𝐙𝐔 𝐊𝐎 𝐑𝐄𝐐𝐔𝐒𝐄𝐓 𝐁𝐇𝐄𝐉 𝐃𝐎  😌👈\n●============================================●\nhttps://www.facebook.com/profile.php?id=61550268698294&mibextid=ZbWKwL\n●============================================●\n\n𝗽𝗿𝗲𝗳𝗶𝘅 => 👉 ${global.config.PREFIX} 👈\n●============================================●\n\𝐧𝐁𝐎𝐓 𝐀𝐏𝐑𝐎𝐕𝐀𝐋 𝐊𝐄 𝐋𝐈𝐘𝐄 ${global.config.PREFIX}𝐑𝐄𝐐𝐔𝐒𝐓 𝐋𝐈𝐊𝐇 𝐊𝐄 𝐒𝐄𝐍𝐃 𝐊𝐀𝐑𝐎  🙂🖐️) 
-`, attachment: fs.createReadStream(__dirname + "/cache/botjoin.mp4")} ,threadID));
+`, attachment: fs.createReadStream(__dirname + "/cache/botjoin.m4")} ,threadID));
   }
   else {
       try {
