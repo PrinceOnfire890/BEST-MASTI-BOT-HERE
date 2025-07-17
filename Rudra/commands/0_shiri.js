c3a94e6cb8751ab4d4a066379843c747516024c4
PrinceOnfire890 <princehere2010@gmail.com>
2025-07-17 19:38:21 +0530

    Update 0_shiri.js

--- Rudra/commands/0_shiri.js
+++ Rudra/commands/0_shiri.js
@@ -11,7 +11,7 @@ module.exports.config = {
   cooldowns: 2
 };
 
-const API_URL = "https://gemini2-y8sx.onrender.com/chat";
+const API_URL = "https://gemini2-gjmw.onrender.com/chat";
 const chatHistories = {};
 let botOn = false; // ✅ Global toggle now
 
